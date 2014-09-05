1e54e98e141b38907d3f4392e088111cc11f17fb
Zaradiet <Zaradiet@users.noreply.github.com>
2014-09-05 14:08:55 +0200

    Update and rename odoo_install.sh to odoo_install2.sh

--- odoo-v8/ubuntu-14-04/odoo_install2.sh
+++ odoo-v8/ubuntu-14-04/odoo_install2.sh
@@ -15,38 +15,6 @@
 # ./odoo-install 
 #
 ################################################################################
- 
-##fixed parameters
-#openerp
-OE_USER="odoo"
-OE_HOME="/opt/$OE_USER"
-OE_HOME_EXT="/opt/$OE_USER/$OE_USER-server"
-
-#Enter version for checkout "8.0" for version 8.0, "7.0 (version 7), saas-4, saas-5 (opendays version) and "master" for trunk
-OE_VERSION="8.0"
-
-#set the superadmin password
-OE_SUPERADMIN="superadminpassword"
-OE_CONFIG="$OE_USER-server"
-
-#--------------------------------------------------
-# Update Server
-#--------------------------------------------------
-echo -e "\n---- Update Server ----"
-sudo apt-get update
-sudo apt-get upgrade -y
-
-#--------------------------------------------------
-# Install PostgreSQL Server
-#--------------------------------------------------
-echo -e "\n---- Install PostgreSQL Server ----"
-sudo apt-get install postgresql -y
-	
-echo -e "\n---- PostgreSQL $PG_VERSION Settings  ----"
-sudo sed -i s/"#listen_addresses = 'localhost'"/"listen_addresses = '*'"/g /etc/postgresql/9.3/main/postgresql.conf
-
-echo -e "\n---- Creating the ODOO PostgreSQL User  ----"
-sudo su - postgres -c "createuser -s $OE_USER" 2> /dev/null || true
 
 #--------------------------------------------------
 # Install Dependencies
